d26c5d1a3950de4ff9d8849e857e51d48a314c28
Faisal Vostro - Ubuntu <faisalakhtar1999@gmail.com>
2021-10-18 14:21:02 +0530

    check

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,6 @@
 const modeCheckBoxEl = document.querySelector('.modeCheckBox');
 let isDarkMode = sessionStorage.getItem('darkMode') == 'true';
 modeCheckBoxEl.checked = isDarkMode;
-init();
 
 function openMenu(arg) {
     arg.classList.toggle('open')
@@ -112,40 +111,38 @@ document.onkeyup = function (e) {
     }
 }
 
-function init () {
-    modeCheckBoxEl.addEventListener('change', function () {
-        switchMode(modeCheckBoxEl.checked)
-    })
+modeCheckBoxEl.addEventListener('change', function () {
+    switchMode(modeCheckBoxEl.checked)
+})
 
-    if (modeCheckBoxEl.checked) {
-        if ('ontouchstart' in document.documentElement) {
-            showPopupMsg("Opened in dark mode<br>Switch to light mode from the menu")
-        } else {
-            showPopupMsg("Opened in dark mode<br>Switch to light mode by pressing 'Alt+M'")
-        }
-        document.querySelector('body').classList.add('dark')
+if (modeCheckBoxEl.checked) {
+    if ('ontouchstart' in document.documentElement) {
+        showPopupMsg("Opened in dark mode<br>Switch to light mode from the menu")
     } else {
-        if ('ontouchstart' in document.documentElement) {
-            showPopupMsg("Opened in light mode<br>Switch to dark mode from the menu")
-        } else {
-            showPopupMsg("Opened in light mode<br>Switch to dark mode by pressing 'Alt+M'")
-        }
-        document.querySelector('body').classList.remove('dark')
+        showPopupMsg("Opened in dark mode<br>Switch to light mode by pressing 'Alt+M'")
     }
-
-    if (sessionStorage.getItem('faisalsSign') == null) {
-        sessionStorage.setItem('faisalsSign', new Date())
-        startWritingName()
+    document.querySelector('body').classList.add('dark')
+} else {
+    if ('ontouchstart' in document.documentElement) {
+        showPopupMsg("Opened in light mode<br>Switch to dark mode from the menu")
     } else {
-        let faisalsSign = sessionStorage.getItem('faisalsSign')
-        faisalsSign = new Date(faisalsSign)
-        faisalsSign = (Date.now() - faisalsSign.getTime()) / 3600000
-        if (faisalsSign > 1) sessionStorage.removeItem('faisalsSign')
-        endWritingName()
+        showPopupMsg("Opened in light mode<br>Switch to dark mode by pressing 'Alt+M'")
     }
-
-    resizeSlider(getMaxSlidesHeight() + "px")
+    document.querySelector('body').classList.remove('dark')
 }
+
+if (sessionStorage.getItem('faisalsSign') == null) {
+    sessionStorage.setItem('faisalsSign', new Date())
+    startWritingName()
+} else {
+    let faisalsSign = sessionStorage.getItem('faisalsSign')
+    faisalsSign = new Date(faisalsSign)
+    faisalsSign = (Date.now() - faisalsSign.getTime()) / 3600000
+    if (faisalsSign > 1) sessionStorage.removeItem('faisalsSign')
+    endWritingName()
+}
+
+resizeSlider(getMaxSlidesHeight() + "px")
 
 window.onresize = function () {
     resizeSlider("unset")
